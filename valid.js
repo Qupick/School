e74ff9d82b16e237da1775986be0cd63b401d866
Taras Soprano <gennrral@gmail.com>
2018-02-15 14:44:28 +0200

    valid select

--- valid.js
+++ valid.js
@@ -7,7 +7,12 @@ function validate() {
  var mail = document.getElementById ("email");
  var pass = document.getElementById ("password");
  var pass2 = document.getElementById ("password_repeat")
-
+ var sel1 = document.getElementById ("nocheckSelect1");
+ var sel2 = document.getElementById ("nocheckSelect2");
+ // var id options selercts one
+ /*var nosel1 = document.getElementById ("nocheckSelect1");
+ var nosel2 = document.getElementById ("nocheckSelect2");
+*/
  /*// Errors message.
  var userEror = document.getElementById ("userError");
  var addressEror = document.getElementById ("addressError");
@@ -16,39 +21,61 @@ function validate() {
  var passEror = document.getElementById ("passError");
  var pass2Eror = document.getElementById ("pass2Error");
 */
-  username = user.value;
+ /* username = user.value;
   if (username === "") {
       document.getElementById ("userError").style.display="block";
-  } else {
-      document.getElementById ("userGood").style.display="block";
+  } else if (username != ""){
+    document.getElementById ("userError").style.display="none";
   };
  
   address = addressa.value;
   if (address === "") {
       document.getElementById ("addressError").style.display="block";
+  } else if (username != ""){
+    document.getElementById ("addressError").style.display="none";
   };
 
   phone = number_phone.value;
   if (phone === "") {
       document.getElementById ("numberError").style.display="block";
+  } else if (phone != "") {
+    document.getElementById ("numberError").style.display="none";
   };
   
   mail = email.value;
   if (mail === "") {
       document.getElementById ("mailError").style.display="block";
+  } else if (mail != "") {
+    document.getElementById ("mailError").style.display="none";
   };
   
   pass2 = password_repeat.value;
   pass = password.value;
   if (pass === "") {
       document.getElementById ("pass1Error").style.display="block";
+  } else if (pass != ""){
+      document.getElementById ("pass1Error").style.display="none";
   } else if (pass != pass2) {
       document.getElementById ("passisnoGood").style.display="block";
-  } else {
-    document.getElementById ("passisGood").style.display="block";
+  } else if (pass === pass2) {
+     document.getElementById ("passisnoGood").style.display="none";
   };
+  */
+  
+  sel1 = nocheckSelect1.value;
+  if (sel1 === "") {
+    document.getElementById ("select1").style.display="block";
+  } else if (sel1 != "") {
+    document.getElementById ("select1").style.display="none";
   };
 
+  sel2 = nocheckSelect2.value;
+  if (sel2 === "") {
+    document.getElementById ("select2").style.display="block";
+  } else if (sel2 != "") {
+    document.getElementById ("select2").style.display="none";
+  };
+  };
 
 
 
