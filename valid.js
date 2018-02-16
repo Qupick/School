99501b07eb73454885a90aebdedf64c348a05142
Taras Soprano <gennrral@gmail.com>
2018-02-16 11:47:34 +0200

    go to finish

--- valid.js
+++ valid.js
@@ -1,5 +1,5 @@
 
-function validate() {
+var Go = function validate() {
  // inputs object
  var user = document.getElementById ("username");
  var addressa = document.getElementById ("address");
@@ -9,38 +9,40 @@ function validate() {
  var pass2 = document.getElementById ("password_repeat")
  var sel1 = document.getElementById ("nocheckSelect1");
  var sel2 = document.getElementById ("nocheckSelect2");
- // var id options selercts one
- /*var nosel1 = document.getElementById ("nocheckSelect1");
+ 
+ // var id options Selects and Payments
+ var nosel1 = document.getElementById ("nocheckSelect1");
  var nosel2 = document.getElementById ("nocheckSelect2");
-*/
- /*// Errors message.
- var userEror = document.getElementById ("userError");
+
+ var over = document.getElementById ("overFinish");
+ // Errors message.
+ /*var userEror = document.getElementById ("userError");
  var addressEror = document.getElementById ("addressError");
  var phoneEror = document.getElementById ("phoneError");
  var mailEror = document.getElementById ("mailError");
  var passEror = document.getElementById ("passError");
- var pass2Eror = document.getElementById ("pass2Error");
-*/
- /* username = user.value;
+ var pass2Eror = document.getElementById ("pass2Error");*/
+
+  username = user.value;
   if (username === "") {
       document.getElementById ("userError").style.display="block";
   } else if (username != ""){
     document.getElementById ("userError").style.display="none";
-  };
+  }; 
  
   address = addressa.value;
   if (address === "") {
       document.getElementById ("addressError").style.display="block";
   } else if (username != ""){
     document.getElementById ("addressError").style.display="none";
-  };
+  }; 
 
   phone = number_phone.value;
   if (phone === "") {
       document.getElementById ("numberError").style.display="block";
   } else if (phone != "") {
     document.getElementById ("numberError").style.display="none";
-  };
+  }; 
   
   mail = email.value;
   if (mail === "") {
@@ -55,12 +57,17 @@ function validate() {
       document.getElementById ("pass1Error").style.display="block";
   } else if (pass != ""){
       document.getElementById ("pass1Error").style.display="none";
-  } else if (pass != pass2) {
+  };  
+  if (pass != pass2) {
       document.getElementById ("passisnoGood").style.display="block";
   } else if (pass === pass2) {
      document.getElementById ("passisnoGood").style.display="none";
   };
-  */
+  if (pass2 === "") {
+    document.getElementById ("pass2Error").style.display="block"
+  }  else if (pass2 != "") {
+    document.getElementById ("pass2Error").style.display="none"
+  };
   
   sel1 = nocheckSelect1.value;
   if (sel1 === "") {
@@ -75,7 +82,22 @@ function validate() {
   } else if (sel2 != "") {
     document.getElementById ("select2").style.display="none";
   };
-  };
+     //========================================================
+            
+     
+
+     var finish = document.getElementById ("overFinish");
+      validate ().value;
+         if (validate () === true) {
+          alert ("finish"); // alert вместо window.local.href="путь к файлу"
+        } else if (validate () != false) {
+          alert ("Заповніть всі поля");
+        };
+             
+      
+  
+  }; // Function validate Registration 
 
 
 
+ 
