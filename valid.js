5082e07f05537e08e928e547c2855da1deaa7194
Taras Soprano <gennrral@gmail.com>
2018-02-18 15:18:29 +0200

    repair ubuntu

--- valid.js
+++ valid.js
@@ -1,5 +1,5 @@
 
-var Go = function validate() {
+var Go = function () {
  // inputs object
  var user = document.getElementById ("username");
  var addressa = document.getElementById ("address");
@@ -13,19 +13,25 @@ var Go = function validate() {
  // var id options Selects and Payments
  var nosel1 = document.getElementById ("nocheckSelect1");
  var nosel2 = document.getElementById ("nocheckSelect2");
-
  var over = document.getElementById ("overFinish");
+
  // Errors message.
- /*var userEror = document.getElementById ("userError");
+ var userEror = document.getElementById ("userError");
  var addressEror = document.getElementById ("addressError");
  var phoneEror = document.getElementById ("phoneError");
  var mailEror = document.getElementById ("mailError");
  var passEror = document.getElementById ("passError");
- var pass2Eror = document.getElementById ("pass2Error");*/
+ var pass2Eror = document.getElementById ("pass2Error");
+ var sel1Eror = document.getElementById ("select1");
+ var sel2Eror = document.getElementById ("select2"); 
+
+    var Errors = ["userError", "addressError", "phoneError", "mailError", 
+    "passError", "pass2Error", "sel1Error", "sel2Error"];
+
 
   username = user.value;
   if (username === "") {
-      document.getElementById ("userError").style.display="block";
+     document.getElementById ("userError").style.display="block";
   } else if (username != ""){
     document.getElementById ("userError").style.display="none";
   }; 
@@ -54,9 +60,9 @@ var Go = function validate() {
   pass2 = password_repeat.value;
   pass = password.value;
   if (pass === "") {
-      document.getElementById ("pass1Error").style.display="block";
+     document.getElementById ("pass1Error").style.display="block";
   } else if (pass != ""){
-      document.getElementById ("pass1Error").style.display="none";
+     document.getElementById ("pass1Error").style.display="none";
   };  
   if (pass != pass2) {
       document.getElementById ("passisnoGood").style.display="block";
@@ -64,40 +70,40 @@ var Go = function validate() {
      document.getElementById ("passisnoGood").style.display="none";
   };
   if (pass2 === "") {
-    document.getElementById ("pass2Error").style.display="block"
+     document.getElementById ("pass2Error").style.display="block"
   }  else if (pass2 != "") {
-    document.getElementById ("pass2Error").style.display="none"
+     document.getElementById ("pass2Error").style.display="none"
   };
   
   sel1 = nocheckSelect1.value;
   if (sel1 === "") {
-    document.getElementById ("select1").style.display="block";
+     document.getElementById ("select1").style.display="block";
   } else if (sel1 != "") {
-    document.getElementById ("select1").style.display="none";
+     document.getElementById ("select1").style.display="none";
   };
 
   sel2 = nocheckSelect2.value;
   if (sel2 === "") {
-    document.getElementById ("select2").style.display="block";
+     document.getElementById ("select2").style.display="block";
   } else if (sel2 != "") {
-    document.getElementById ("select2").style.display="none";
+     document.getElementById ("select2").style.display="none";
   };
      //========================================================
-            
-     
+      
 
-     var finish = document.getElementById ("overFinish");
-      validate ()= true.value;
-         if (validate () === true) {
-          alert ("finish"); // alert вместо window.local.href="путь к файлу"
-        } else if (validate () != true) {
-          alert ("Заповніть всі поля");
-        };
-             
+     
+       Errors = true.value
+      if (Errors === true) {
+     alert ("no");
+    } else if (Errors != true) {
+      alert ("gooo");
+    } 
+
       
   
   }; // Function validate Registration 
 
 
 
+   
  
