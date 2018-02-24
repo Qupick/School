e93d43c931f210c3413f00949e52305c4d63b685
Soprano <gennrral@gmail.com>
2018-02-24 17:18:43 +0200

    Errors input

--- valid.js
+++ valid.js
@@ -14,8 +14,7 @@ var Go = function () {
 
     var Errors = [];
     
-
-   
+	   
     username = user.value;
   if (username === ""){ 
     Errors.push("userError");
@@ -24,15 +23,16 @@ var Go = function () {
    };
 
 
-var reg = .match /\d/g;
+  var reg = /^[A-z]+$/;
      
+  var result = username.search(reg);
 
-
-    username != reg.value;
-   if (username === reg) {
+  
+   if (result === -1) {
     Errors.push("noName");
-   document.getElementById("noName").style.display="none"
-    }
+    } else {
+      document.getElementById("noName").style.display="none"
+    };
      
 
 
@@ -43,13 +43,12 @@ var reg = .match /\d/g;
    };
   };
 
-  if (username.length < 4) {
+     if (username.length < 4) {
     for (var i=4; i > username.length; --i){
       Errors.push("noName");
       document.getElementById("noName").style.display="none"
     };
   };
-
 
 
 
@@ -62,13 +61,26 @@ var reg = .match /\d/g;
   if (address.length > 255) {
     for (var i=0; i < address.length; i++){
       Errors.push ("noAddressGood");
-      document.getElementById("noAddressGood").style.display="none"
     };
-  };
+  }else{
+     document.getElementById("noAddressGood").style.display="none"
+   };
    
      
-  
+     var regex = /^[0-9]+$/;
+     
+     var result2 = number_phone.search(regex);
 
+     console.log(result2);
+
+     if (result2 === -1) {
+      Errors.push("phoneError");
+     }else{
+      document.getElementById("phoneError").style.display="none"
+     };
+  
+  
+  
     phone = number_phone.value;
   if (phone === "") {
      Errors.push("numberError");
@@ -76,6 +88,7 @@ var reg = .match /\d/g;
      document.getElementById("numberError").style.display="none"
    };
   
+
 
     mail = email.value;
   if (mail === "") {
@@ -85,6 +98,21 @@ var reg = .match /\d/g;
    };
   
 
+
+    var reg1 = /^[0-9A-za-z]+@[a-z]+[.][a-z]+$/;
+    var result1 = mail.search(reg1);
+
+
+
+    
+    if (result1 === -1){
+      Errors.push("noMail");
+    } else {
+      document.getElementById("noMail").style.display="none"
+    };
+
+
+
     pass = password.value;
   if (pass === "") {
      Errors.push("pass1Error");
@@ -93,18 +121,19 @@ var reg = .match /\d/g;
    };
 
 
+  
     if (pass.length < 6) {
       for (var i=6; i > pass.length; --i){
         Errors.push("pass1NoGood");
-        document.getElementById("pass1NoGood").style.display="none"
-      };
+      }}else{
+      document.getElementById("pass1NoGood").style.display="none"
     };
 
     if (pass.length > 100) {
       for (var i=0; i < pass.length; i++){
         Errors.push("pass1NoGood");
-        document.getElementById("pass1NoGood").style.display="none"
-      };
+      }}else{
+       document.getElementById("pass1NoGood").style.display="none"
     };
 
   
